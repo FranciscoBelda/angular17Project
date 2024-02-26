911312ac8c0b74589769cfb4657e39f28149abd3
FranciscoBelda <francisco.belda@cieep.com>
2024-02-26 20:39:24 +0100

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular17Project';
+  title = 'Rick y Morty Project';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {CharListComponent} from "./components/char-list/char-list.component";
+import {CharDetailComponent} from "./components/char-detail/char-detail.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/characters/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'characters/list',
+    component: CharListComponent,
+  },
+  {
+    path: 'characters/detail/:id',
+    component: CharDetailComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/characters/list',
+    pathMatch: 'full'
+  },
+];
